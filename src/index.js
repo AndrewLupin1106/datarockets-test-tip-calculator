743b94ad6e6d71b3ba02937dae58b25eb6048aee
apollo <apollo@a.com>
2023-04-24 17:21:40 +0200

    Andrew Lupin finish

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
-/**
-=========================================================
-* Material Kit 2 React - v2.1.0
-=========================================================
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+import AppProvider from './context';
 
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-import React from "react";
-import * as ReactDOMClient from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "App";
-
-const container = document.getElementById("root");
-
-// Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
